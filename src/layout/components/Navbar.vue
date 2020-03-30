9b7262d17df4be701070b5850b7312da4d1743fa
ofh <l>
2020-03-30 09:56:53 +0800

    添加eslint修复工具

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -7,7 +7,7 @@
     <div class="right-menu">
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
-          {{name}}
+          {{ name }}
           <img :src="avatar+'?imageView2/1/w/80/h/80'" class="user-avatar">
           <i class="el-icon-caret-bottom" />
         </div>
