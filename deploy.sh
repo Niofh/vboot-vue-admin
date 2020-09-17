fa38c30fdc2ec348b259a3a0c25e8b0c1b58a059
oufh <888888>
2020-09-17 15:36:06 +0800

    修复命令错误

--- deploy.sh
+++ deploy.sh
@@ -35,5 +35,5 @@ rm -rf usr/local/java/nginx-vboot/www/*
 
 
 echo "============复制dist下的文件到/usr/local/java/nginx-vboot下的www文件============="
-cp -r /usr/local/java/vboot/vboot-vue-admin/dist/.  /usr/local/java/nginx-vboot/www
+cp -r /usr/local/java/vboot/vboot-vue-admin/dist/  /usr/local/java/nginx-vboot/www
 
